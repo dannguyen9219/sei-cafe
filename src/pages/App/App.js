148bad141997ee43b5f2c858f9f9f0e442e6f81f
nguyen.danimal@gmail.com <endlesswaltz@MacBook-Pro-2.local>
2022-04-06 21:31:43 -0500

    added client-side routing

---
+++ src/pages/App/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'; // using curly braces because it is a named function in React
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import AuthPage from '../AuthPage/AuthPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+
+  const [user, setUser] = useState(null); // useState returns an array, so we are destructuring on the left; value of null will be stored in the first item in the array // w18d02 2:35 take lots of notes // null is falsy value
+
+
+  return (
+    <main className="App">
+      {
+        user ?
+        <Routes>
+          <Route path="/orders/new" element={<NewOrderPage/>}></Route>
+          <Route path="/orders" element={<OrderHistoryPage/>}></Route>
+        </Routes>
+        :
+        <AuthPage />
+      }
+    </main>
+  );
+};
+// If there is no user, you cannot see the NewOrderPage. It is impossible. user ? means that if user is NOT falsy becausee we put useState(null).
+
+export default App;
