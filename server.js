e0aec36985d9c9729c4ee25cae1c2f0b9ed05599
nguyen.danimal@gmail.com <endlesswaltz@MacBook-Pro-2.local>
2022-04-13 20:59:35 -0500

    w19d02 - signup form working

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+require('dotenv').config(); // Make sure all the variables in .env are accessible to us; this will read all that is in .env //
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require ('morgan');
 const PORT = process.env.PORT || 3001 // because frontend is on 3000
 
+// Connect to the database
+require('./config/database.js'); // this runs the database file
 
 const app = express();
 
@@ -11,7 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico'))); // Use this so that it doesn't matter what OS you use // directory name - current directory you are inside - value of directory you are currently inside - Mern-infrastructure // this is to have a favicon on our website
 
-app.use(express.static(path.join(__dirname, 'build'))); // want to static the build folder because it is the production ready code to deploy; this is what we want to serve; static means anything in this folder I want to serve as is //
+app.use(express.static(path.join(__dirname, 'build'))); // want to static the build folder because it is the production ready code to deploy; this is what we want to serve; static means anything in this folder I want to serve as is // it will also figure out what computer you are on, and it will figure out the path
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
