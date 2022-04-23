0f123a41a4e05fde3bdfb3ab0f186206e644ba50
nguyen.danimal@gmail.com <endlesswaltz@MacBook-Pro-2.local>
2022-04-23 10:46:23 -0500

    cloned mern infrastructure, set up environment

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'; // using curly braces because it is a named fu
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import AuthPage from '../AuthPage/AuthPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import './App.css';
+import './App.module.css';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
